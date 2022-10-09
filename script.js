68e6d7e1d320bde432f0ca15d5e795593e283552
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-09 00:39:48 -0400

    fixed errors amd bugs

---
+++ script.js
@@ -0,0 +1,67 @@
+// Type the illegal character in this statement.
+var productCost = 3.45;
+//productcost
+
+// Type the illegal character in this statement.
+// var P2_a%88$;
+//%
+
+// Rewrite this using camelCase.
+var Nameofband;
+var nameOfBand;
+
+// Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them.
+var $_;
+var _$;
+
+// Declares a variable in camelCase.
+var averagenItelligenceHuman;
+
+// In a single statement, declare the variable firstLegalChar and assign it a string. The string consists entirely of the first character on the keyboard in the top row that's legal in a variable name, as long as the character doesn't come first.
+var firstLegalChar = "1";
+
+// Declare a variable, in camelCase, that combines the two syllables boo and hoo.
+var booHoo;
+
+// In a single statement declare a legally-named variable and assign a number to it.
+var leaglNum = 1;
+
+// In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+var leaglVar 
+leaglVar = cost + time;
+                            
+// Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+var _9w;
+
+// The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+var boogieWoogie = "woogieboogie";
+
+// Declare a variable that is a combination of your first and last names. Use camelCase.
+var fnameLname = JustinJones;
+
+// Time goal: 20 seconds. Declare a variable using nothing but all of the legal characters that aren't alphabet characters or numbers. Don't repeat any of them. (The timer starts when you click in the answer field.)
+var _$;
+
+// Time goal: 20 seconds. In a single statement declare a legally-named variable and assign a number to it.
+var cost = 10;
+
+// Time goal: 30 seconds. In a single statement declare a legally-named variable and assign to it the sum of 2 other legally-named variables.
+var legalvar = sum + total;
+
+// Time goal: 25 seconds. Declare a variable whose name is the combination of 3 characters, in this order: a legal first character that is not a letter, a character that would be illegal as a first character, and a character that would be legal anywhere in the name.
+var _9w;
+
+// Time goal: 30 seconds. The variable boogieWoogie has already been declared. Assign to it the value of another variable, woogieBoogie.
+var boogieWoogie = woogieBoogie;
+
+// Time goal: 20 seconds. Declare a variable that is a combination of your first and last names. Use camelCase. If you have a complex name, simplify it. Use "Sarah Billings" instead of "Sarah Sue Billings-Coopersmith III"
+var justinEmanuelJones;
+
+// Declare a variable whose name is your first and last names combined, in camelCase.
+// Assign the variable your first and last names, as a string.
+// Code an alert, specifying the variable, not the string, as the message.
+// Click the Run It button to run your code live.
+
+var justinEmanuelJones;
+justinEmanuelJones = "Justin" + " Jones";
+alert(justinEmanuelJones);
